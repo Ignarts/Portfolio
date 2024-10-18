df58748647f86e6616af0cc0cf7e7d442c7dbc8d
Ignacio Meléndez <ignaciomelendezuriz@gmail.com>
2024-10-19 00:54:38 +0200

    Try bug on Github

--- Assets/Slime/SlimeBackground.js
+++ Assets/Slime/SlimeBackground.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const container = document.querySelector(".chat-window");
     const bgImage = new Image();
-    bgImage.src = "Assets/Slime/Images/Background.png";
+    bgImage.src = "/Assets/Slime/Images/Background.png";
 
     bgImage.onload = function() {
         container.style.width = bgImage.width + "px";
