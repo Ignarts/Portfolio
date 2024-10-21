dfc06ababd1737d4b80f7664a63a0cbea72942c5
Ignacio Meléndez <ignaciomelendezuriz@gmail.com>
2024-10-21 11:25:54 +0200

    Configure The Article Base

---
+++ Articles/Scripts/ArticleToggleMode.js
@@ -0,0 +1,71 @@
+let isLightMode = false;
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    isLightMode = true;
+    toggleMode();
+});
+
+const toggleMode = () => {
+    if (isLightMode) {
+        ChangeToDarkMode();
+        AnimateButtonToDarkMode();
+    } else {
+        ChangeToLightMode();
+        AnimateButtonToLightMode();
+    }
+    console.log('Mode changed to: ' + (isLightMode ? 'Light' : 'Dark'));
+}
+
+const ChangeToLightMode = () => {
+    // Change body style to light
+    document.body.classList.remove('body-Dark');
+    document.body.classList.add('body-Light');
+
+    // Change header to light
+    const header = document.querySelector('header');
+    header.classList.add('header-Light');
+    header.classList.remove('header-Dark');
+}
+
+const ChangeToDarkMode = () => {
+    // Change body style to light
+    document.body.classList.remove('body-Light');
+    document.body.classList.add('body-Dark');
+
+    // Change header to light
+    const header = document.querySelector('header');
+    header.classList.add('header-Dark');
+    header.classList.remove('header-Light');
+}
+
+/// Animate button to show light mode
+const AnimateButtonToLightMode = () => {
+    const modeButton = document.getElementById('modeButton');
+    const modeImage = document.getElementById('modeImage');
+
+    modeImage.src = '../../Assets/Resources/Images/moon.svg';
+    isLightMode = true;
+
+    modeImage.classList.remove('rotate-Negative90');
+    modeImage.classList.add('rotate-90');
+    modeButton.classList.remove('lightMode');
+    modeButton.classList.add('darkMode');
+
+    isLightMode = true;
+}
+
+/// Animate button to show dark mode
+const AnimateButtonToDarkMode = () => {
+    const modeButton = document.getElementById('modeButton');
+    const modeImage = document.getElementById('modeImage');
+
+    modeImage.src = '../../Assets/Resources/Images/brightness-high.svg';
+    isLightMode = false;
+
+    modeImage.classList.remove('rotate-90');
+    modeImage.classList.add('rotate-Negative90');
+    modeButton.classList.add('lightMode');
+    modeButton.classList.remove('darkMode');
+
+    isLightMode = false;
+}
