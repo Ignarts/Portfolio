fee36ad89bff25a9e8ee7a8d2084d278f10faeba
Ignacio Meléndez <ignaciomelendezuriz@gmail.com>
2024-10-23 17:23:49 +0200

    Update BlogArticleCreator.js to add links to the blog cards

--- Assets/JS/BlogArticleCreator.js
+++ Assets/JS/BlogArticleCreator.js
@@ -7,40 +7,17 @@ document.addEventListener('DOMContentLoaded', function() {
             imgSrc: 'Assets/Resources/OttersHell/OHGameplay_Card.png',
             imgAlt: 'ottersHell',
             type: 'Games',
-        },
-        {
-            title: 'Otter\'s Hell',
-            date: '10-10-2024',
-            description: 'A Roguelite of otters vs hamsters',
-            imgSrc: 'Assets/Resources/OttersHell/OHGameplay_Card.png',
-            imgAlt: 'ottersHell',
-            type: 'Unity',
-        },
-        {
-            title: 'Otter\'s Hell',
-            date: '10-10-2024',
-            description: 'A Roguelite of otters vs hamsters',
-            imgSrc: 'Assets/Resources/OttersHell/OHGameplay_Card.png',
-            imgAlt: 'ottersHell',
-            type: 'Tools',
-        },
-        {
-            title: 'Otter\'s Hell',
-            date: '10-10-2024',
-            description: 'A Roguelite of otters vs hamsters',
-            imgSrc: 'Assets/Resources/OttersHell/OHGameplay_Card.png',
-            imgAlt: 'ottersHell',
-            type: 'Devlog',
+            link: '../../Articles/HTML/Blog/Bg_OttersHell.html'
         }
     ]
 
-    function createBlogArticle(article)
-    {
+    function createBlogArticle(article){
+        const link = article.link ? `onclick="window.location.href='${article.link}'"` : '';
         return `
-            <article class="blog-card">
+            <article class="blog-card" ${link}>
                     <div class="blog-card-img">
                         <p class="blog-card-tag ${article.type}-Tag " id="colorElement"><strong>${article.type}</strong></p>
-                        <img src="${article.imgSrc}" alt="ottersHell">
+                        <img src="${article.imgSrc}" alt="${article.imgAlt}">
                     </div>
                     <div class="blog-card-content">
                         <h4 class="date">${article.date}</h4>
